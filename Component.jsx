218a2bca8aef120c6730439622ad499afb426e57
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-01-29 13:44:33 -0600

    isomorphic app is now passing props from the server to the client

--- Component.jsx
+++ Component.jsx
@@ -2,18 +2,20 @@ var React = require('react')
 
 module.exports = React.createClass({
     render () {
+        const { title } = this.props
         return (
             <html>
                 <head>
-                    <title>Isomorphic App with React</title>
+                    <title>{ title }</title>
                     <link rel="stylesheet" href="/style.css" />
                 </head>
                 <body>
                     <div>
-                        <h1>Hello World!</h1>
+                        <h1>{ title }</h1>
                         <p>server side rendering baby</p>
                         <button onClick={ this._handleClick }>Press me for proof!</button>
                     </div>
+                    <script dangerouslySetInnerHTML={ {__html: 'window.PROPS=' + JSON.stringify(this.props)} }/>
                     <script src="/bundle.js" />
                 </body>
             </html>
