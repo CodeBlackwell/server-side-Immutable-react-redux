19cc1ff50deee8bde84ceb116e8e9908bfd03e0f
LeChristopher Blackwell <codeblackwell@gmail.com>
2017-01-29 13:31:53 -0600

    made additions necessary for creating an MVP "universal/isomorphic" application

--- Component.jsx
+++ Component.jsx
@@ -3,11 +3,21 @@ var React = require('react')
 module.exports = React.createClass({
     render () {
         return (
-            <div>
-                <h1>Hello World!</h1>
-                <p>server side rendering baby</p>
-                <button onClick={ this._handleClick }>Press me for proof!</button>
-            </div>
+            <html>
+                <head>
+                    <title>Isomorphic App with React</title>
+                    <link rel="stylesheet" href="/style.css" />
+                </head>
+                <body>
+                    <div>
+                        <h1>Hello World!</h1>
+                        <p>server side rendering baby</p>
+                        <button onClick={ this._handleClick }>Press me for proof!</button>
+                    </div>
+                    <script src="/bundle.js" />
+                </body>
+            </html>
+
         )
     },
 
